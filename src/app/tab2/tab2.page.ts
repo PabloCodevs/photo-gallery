361536df1f78735e004d6f598a9d7724a6ed680d
PabloCodevs <pcavero.dev@gmail.com>
2026-08-23 17:50:04 +0200

    feat: añade PhotoService y conecta el botón de la cámara en Tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,35 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { 
-  IonHeader, 
-  IonToolbar, 
-  IonTitle, 
-  IonContent, 
-  IonFab, 
-  IonFabButton, 
-  IonIcon 
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonFab,
+  IonFabButton,
+  IonIcon,
 } from '@ionic/angular';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { addIcons } from 'ionicons';
-import { camera } from 'ionicons/icons';
+import { camera, images } from 'ionicons/icons';
+import { PhotoService} from '../services/photo.service';
+import { Photo } from '@capacitor/camera';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [
-    IonHeader, 
-    IonToolbar, 
-    IonTitle, 
-    IonContent, 
-    ExploreContainerComponent, 
-    IonFab,
-    IonFabButton,
-    IonIcon
-  ]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonFab, IonFabButton, IonIcon]
 })
+
 export class Tab2Page {
-  constructor() {
-    // Registramos el icono de la cámara para que Ionic lo muestre correctamente
-    addIcons({ camera });
+
+  // Inyectamos el servicio directamente en el constructor usando 'public'
+  constructor(public photoService: PhotoService) {
+    // Registramos los iconos para que Ionic los muestre correctamente
+    addIcons({ camera, images });
+  }
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
   }
 }
